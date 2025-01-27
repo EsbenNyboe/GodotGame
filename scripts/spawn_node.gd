b042c7d7a6b8a1a87be18f180f5418c67c77f83e
EsbenNyboeKanda <enj@kanda.dk>
2025-01-27 20:37:08 +0100

    Randomize x-position

--- scripts/spawn_node.gd
+++ scripts/spawn_node.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 var mynode = preload("res://scenes/spawnable.tscn")
 
+const RANDOM_RANGE = 5
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("mouse_button"):
 		inst(get_global_mouse_position())
 
 func inst(pos):
 	var instance = mynode.instantiate()
+	pos.x += randf() * RANDOM_RANGE - RANDOM_RANGE * 0.5
 	instance.position = pos
 	add_child(instance)
