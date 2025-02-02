d24f49d844f30e65b3358ec02ef0d42b62ddfe74
EsbenNyboeKanda <enj@kanda.dk>
2025-02-02 14:38:56 +0100

    Add dummy-code for running copy-logic

--- scripts/copiable_tile.gd
+++ scripts/copiable_tile.gd
@@ -10,4 +10,8 @@ const TILE_SIZE = 16
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var mouse_pressed = Input.is_action_pressed("mouse_button_left")
-	highlightable.set_highlight(hoverable.is_hovered(mouse_position, position, offset.position) and !mouse_pressed)
+	var is_hovering = hoverable.is_hovered(mouse_position, position, offset.position)
+	highlightable.set_highlight(is_hovering and !mouse_pressed)
+	
+	if Input.is_action_just_pressed("mouse_button_left") and is_hovering:
+		print("copy now")
